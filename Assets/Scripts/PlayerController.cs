d95e7ad610326c9e9449450b67040307975eb3a8
Istvan Lajtar <istvan.lajtar@gmail.com>
2021-10-09 15:25:09 +0200

    Displaying Score And Text

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,28 +2,29 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using TMPro;
 
 public class PlayerController : MonoBehaviour
 {
     public float speed = 10.0f;
+    public TextMeshProUGUI countText;
+    public GameObject winText;
 
     private Rigidbody rb;
 
     private float movementX;
     private float movementY;
 
+    private int count;
+
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>();
-    }
+        count = 0;
 
-    private void OnMove(InputValue movementValue)
-    {
-        Vector2 movementVector = movementValue.Get<Vector2>();
-
-        movementX = movementVector.x;
-        movementY = movementVector.y;
+        SetCountText();
+        winText.SetActive(false);
     }
 
     private void FixedUpdate()
@@ -38,6 +39,26 @@ public class PlayerController : MonoBehaviour
         if (other.gameObject.CompareTag("PickUp"))
         {
             other.gameObject.SetActive(false);
-        }        
+            count++;
+
+            SetCountText();
+        }
     }
+
+    void OnMove(InputValue movementValue)
+    {
+        Vector2 movementVector = movementValue.Get<Vector2>();
+
+        movementX = movementVector.x;
+        movementY = movementVector.y;
+    }
+
+    void SetCountText()
+    {
+        countText.text = "Count: " + count.ToString();
+        if (count >= 9)
+        {
+            winText.SetActive(true);
+        }
+    }        
 }
